c1824cc3f90c734b4e8af8232e5823024f230a07
piotrmackiewiczstryber <piotr.mackiewicz@stryber.io>
2024-01-11 11:11:53 +0100

    x

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/piotrmackiewicz/Documents/projects/stryber/phone-number-input-issue-demo/src/app/layout":["static/media/c22ccc5eb58b83e1-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
